8a5c8dabc68d7c3351a6d6d1772862eae7a70b57
Trung Peter Ho <60043519+tpeterho@users.noreply.github.com>
2022-06-25 21:36:58 -0700

    Update lab8_ec_soln.py

--- lab8_ec_soln.py
+++ lab8_ec_soln.py
@@ -2,7 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # set initial values
-
 m = 0.1 #kg
 k = 1000.0 # N/m
 x0 = 1.0
@@ -10,22 +9,18 @@ v0 = 0.0 # m/s
 time = 1.0 # seconds
 
 # Numerical Setup
-
 dt = 0.00001 # time step in seconds
 n = int(round(time/dt))
-
 t = np.zeros([n])
 x = np.zeros([n])
 v = np.zeros([n])
 
 # Initialize values (set initial conditions)
-
 t[0] = 0.0
 x[0] = x0
 v[0] = v0
 
 # Solve the equations
-
 for i in range(n-1):
     F = -k*x[i] - 0.05*v[i]*abs(v[i])
     a = F / m
@@ -37,3 +32,4 @@ plt.plot(t,x)
 plt.xlabel('Time (s)')
 plt.ylabel('Displacement (m)')
 plt.title('Spring-mass Oscillation under Damped Force')
+plt.show()
